c73ac44c8b83300bf866c2dc15a8e5b465f6bf0b
Julius <JuliusDeBoer@users.noreply.github.com>
2021-10-31 20:37:19 +0100

    Move file to js folder

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 console.info("(*-*)");
 
 var script = document.createElement('script');
-script.src = "lang.js";
+script.src = "js/lang.js";
 script.type = 'text/javascript';
 document.head.appendChild(script);
 
