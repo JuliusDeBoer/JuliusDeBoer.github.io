02303c7604e6c81d88b8976963639854cbbb526e
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2023-10-21 16:43:42 +0200

    style: Disable scrollbars

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
-	<div class="bg-black w-full min-h-screen h-full flex justify-center items-center">
+	<div class="bg-black w-full h-screen flex justify-center items-center overflow-hidden">
 		<cube />
 	</div>
 </template>
