96e7591011df137bd957e7b2809d5b0dd559b765
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2023-10-30 12:06:33 +0100

    fix: Set color mode z-index

--- app.vue
+++ app.vue
@@ -28,9 +28,9 @@
 <template>
 	<div class="min-w-full min-h-screen bg-background-100 text-text-900 dark:bg-background dark:text-text">
 
-		<div class="fixed w-full">
+		<div class="fixed w-full z-50">
 			<div class="container mx-auto flex flex-col justify-center items-end p-2">
-				<label class="btn btn-ghost btn-circle swap swap-rotate">
+				<label class="btn btn-neutral btn-circle swap swap-rotate">
 					<input type="checkbox" v-model="colorToggleValue" @input="updateColorscheme" />
 
 					<Icon name="uil:moon" class="swap-on w-10 h-10" />
