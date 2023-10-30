76c1c0fe4a9744e939987e09ef6166b420190cf2
Julius de Boer <45075461+JuliusDeBoer@users.noreply.github.com>
2023-10-30 12:33:30 +0100

    feat: Add sitemap

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,12 +12,23 @@ export default defineNuxtConfig({
 		"@nuxtjs/tailwindcss",
 		"@nuxtjs/robots",
 		"@nuxtjs/color-mode",
-		"nuxt-icon"
+		"nuxt-icon",
+		"nuxt-simple-sitemap"
 	],
 	devtools: {
 		enabled: true
 	},
 	colorMode: {
 		classSuffix: ""
+	},
+	site: {
+		url: "https://juliusdeboer.github.io"
+	},
+	robots: {
+		rules: {
+			UserAgent: "*",
+			Disallow: "",
+			Sitemap: "https://juliusdeboer.github.io/sitemap.xml"
+		}
 	}
 });
